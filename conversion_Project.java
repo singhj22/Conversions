05c5c01c23a9522d20c1588bbd9021bcdd4e3adf
singhj22 <42975589+singhj22@users.noreply.github.com>
2019-01-28 16:07:19 -0500

    Add files via upload

--- conversion_Project.java
+++ conversion_Project.java
@@ -8,24 +8,27 @@ public class conversion_Project
 
 	public static void main(String[] args) 
 	{
-
+		System.out.println(feetToInches(1));
+		System.out.println(milesToFeet(1));
+		System.out.println(yardsToMeters(1.094));
+		System.out.println(poundsToKilograms(2.205));
 	}
-	public static int Feet_to_Inches(int feet)
+	public static double feetToInches(double feet)
 	{
-		int inch= feet*12;
+		double inch= feet*12;
 		return inch;
 	}
-	public static int Miles_to_Feet(int miles)
+	public static double milesToFeet(double miles)
 	{
-		int feet= 5280*miles;
+		double feet= 5280*miles;
 		return feet;
 	}
-	public static double Yards_to_Meters(int yards)
+	public static double yardsToMeters(double yards)
 	{
 		double meters= yards/1.094;
 		return meters;
 	}
-	public static double Pounds_to_Kilograms(int pounds)
+	public static double poundsToKilograms(double pounds)
 	{
 		double kilograms= pounds/2.205;
 		return kilograms;
